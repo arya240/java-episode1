4f629b3e21eed7e49ff346a7e0d22d4eec51b487
arya240 <75195337+arya240@users.noreply.github.com>
2021-10-21 23:53:19 +0700

    Add files via upload

---
+++ java-dasar1/javadasar1.java
@@ -0,0 +1,24 @@
+public class javadasar1{
+    public static void main(String[] args){
+        String first_name, last_name, phone_number, address, sex, email;
+        int age;
+        //double phone_number;
+
+        first_name = "Arya ";
+        last_name = "Fikri Alamsyah";
+        age = 21;
+        phone_number = "085899624423";
+        address = "bekasi, cibarusah";
+        sex = "Male";
+        email = "daweralamsyah12@gmail.com";
+
+
+        System.out.println("Name:" + first_name + last_name + "\n" + "Age:" + age + "y.o" + "\n" + "Phone:"
+                + phone_number + "\n" + "Address:" + address + "\n" + "Sex:" + sex + "\n" + "Email:" + email );
+
+
+
+        
+    } 
+}
+
